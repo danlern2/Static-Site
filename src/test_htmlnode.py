41d9264c3befee56028bd242519964dbd52b4e94
danlern2 <danlern2@gmail.com>
2024-06-09 12:51:43 -0700

    not very happy with these changes. made the mk to textnode function, but its half assed. same for the delimiter split function

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,11 @@
 import unittest
-
 from htmlnode import HTMLNode 
 from htmlnode import LeafNode
+from htmlnode import ParentNode
+from textnode import TextNode
+from htmlnode import text_node_to_html_node
 class TestHTMLNode(unittest.TestCase):
-    
-    
+      
     # def testRepr(self):
     #     new_node = HTMLNode(value=["diiick"], props={"href": "https://www.google.com", "target": "_blank"})
     #     #new_node.__repr__()
@@ -15,15 +16,48 @@ class TestHTMLNode(unittest.TestCase):
     #     new_node2 = HTMLNode(tag="h1", props={"href": "https://www.google.com", "target": "_blank", "biggus": "dickus"})
     #     print(new_node2.props_to_html())
 
-    def testLeafNode(self):
-        leaf = LeafNode(tag="h1", value="dongle", props={"href": "https://www.google.com", "target": "_blank"})
-        print(leaf.to_html())
-    def testLeafNode2(self):
-        leaf2 = LeafNode(value="dongle", props={"href": "https://www.google.com"})
-        print(leaf2.to_html())
-    def testLeafNode3(self):
-        leaf3 = LeafNode("p", "dongle", props={"href": "https://www.google.com", "target": "_blank"})
-        print(leaf3.to_html())
+    # def testLeafNode(self):
+    #     leaf = LeafNode(tag="h1", value="dongle", props={"href": "https://www.google.com", "target": "_blank"})
+    #     print(leaf.to_html())
+    # def testLeafNode2(self):
+    #     leaf2 = LeafNode(value="dongle", props={"href": "https://www.google.com"})
+    #     print(leaf2.to_html())
+    # def testLeafNode3(self):
+    #     leaf3 = LeafNode(None, "my value", props={"href": "https://www.google.com"})
+    #     print(leaf3.to_html())
+
+    # def testParentNode(self):
+    #     parent = ParentNode(
+    # "1st",
+    # [
+    #     LeafNode("b", "Bold text"),
+    #     LeafNode(None, "Normal text"),
+    #     ParentNode(
+    # "nest",
+    # [
+    #     LeafNode("b", "Bold"),
+    #     LeafNode(None, "Normal"),
+    #     LeafNode("i", "italic"),
+    #     LeafNode(None, "blah", props={"href": "www.dick.com", "named": "yoni"}),
+    #     LeafNode("s", "Normal text yet slightly off kilter", props={"href": "www.dick.com", "named": "yoni"}),
+    #     LeafNode("k", "dongle", props={"href": "www.google.com"})
+    # ]
+    # ),
+    #     #ParentNode(None,[LeafNode("i", "italic text")]),
+    #     #ParentNode("p", []),
+    #     ParentNode("p",[LeafNode("i", "italic text")], props={"href": "www.dan.com", "named": "the_man"}),
+    #     LeafNode("i", "italic text"),
+    #     LeafNode(None, "blahblah blah", props={"href": "https://www.dick.com", "named": "yoni"}),
+    #     LeafNode("s", "Normal text yet slightly off kilter", props={"href": "https://www.dick.com", "named": "yoni"}),
+    #     LeafNode("k", "dongle", props={"href": "https://www.google.com", "target": "_blank"})
+    # ]
+    # )
+    #     print(parent.to_html())
+    
+    def test_eq2(self):
+        node3 = TextNode("This is a text node", "italic", "http://localhost:8888/")
+        node4 = TextNode("This is a text node but wrong", "italic")
+        print(str(text_node_to_html_node(node3)))
 
 
 if __name__ == "__main__":
--- src/textnode.py
+++ src/textnode.py
@@ -1,6 +1,41 @@
+from enum import Enum
+import sys
+#from htmlnode import Type_Rules
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+type_rules = [
+"text",
+"bold",
+"italic",
+"code",
+"link",
+ "image"]    
+delimiters = {
+"bold": "**",
+"italic": "*",
+"code_block": "```",
+"code": "`",
+"link": "[",
+"image": "!",
+"heading": "#",
+"strikethrough": "~~" }
+delimiters2 = {
+"**": "bold",
+"*": "italic",
+"```": "code_block",
+"`": "code",
+"[": "link",
+"!": "image",
+"#": "heading",
+"~~": "strikethrough" }
+
 class TextNode():
-    def __init__(self, content, text_type, url=None):
-        self.text = content
+    def __init__(self, text, text_type, url=None):
+        self.text = text
         self.text_type = text_type
         self.url = url
     
@@ -9,3 +44,67 @@ class TextNode():
             return True
     def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+def mk_to_textnode(text):
+    nodes = []
+    if bool(text) is False or text is None:
+            sys.exit
+    for delim in delimiters2.keys():
+            if delim not in text:
+                continue
+            else:
+                text.find(delim)
+                new_string = text[:text.find(delim)]
+                if bool(new_string) == True:
+                    nodes.append((TextNode(new_string, "text")))
+                delim_string = text[text.find(delim)+1:text.rfind(delim)]
+                nodes.append((TextNode(delim_string.strip(delim), delimiters2.get(delim))))
+                mk_to_textnode(delim_string)
+                after_delim_string = text[text.rfind(delim)+1:]
+                mk_to_textnode(after_delim_string[after_delim_string.rfind(delim)+1:])
+                if bool(after_delim_string) == True:
+                    nodes.extend(mk_to_textnode(after_delim_string.lstrip(delim)))
+                return nodes
+    nodes.append(TextNode(text, "text"))
+    return nodes
+
+###Needs work 
+def text_nodes_delimiter(old_nodes, text_type, *delimiter):
+    new_nodes = []
+    for node in old_nodes:
+        if isinstance(node, TextNode):
+            if text_type is not text_type_text:
+                print(f"this is the print: {node.text}")
+                split = node.text.split(delimiters.get(text_type))
+                if bool(split[0]) == True:
+                    new_nodes.append(TextNode(split[0], text_type_text))
+                new_nodes.append(TextNode(split[1], text_type))
+                if bool(split[2]) == True:
+                    new_nodes.append(TextNode(split[2], text_type_text))
+            else:
+                new_nodes.append(node)
+    return new_nodes
+###
+
+
+def test():
+    mkstring = "This is text with a ***bolded and not italicized*** sentence"
+    another_string = "`this is a code string`"
+    another_string2 = "this is a ~~wrong~~ correct string"
+    another_string3 = "`this is a ~~wrong~~ correct string in code`"
+    nodelist = [TextNode("This is text with a **bolded and not italicized** sentence", "bold")]
+    #TextNode("`this is a code string`", "code"),]
+    #TextNode("this is a ~~wrong~~ correct statement: Yoni sucks", "strikethrough")]
+    # print(mk_to_textnode(mkstring))
+    # print(mk_to_textnode(another_string))
+    # print(mk_to_textnode(another_string2))
+    # print(mk_to_textnode(another_string3))
+    print(text_nodes_delimiter(nodelist, text_type_bold))
+test()
+
+
+
+
+        
+
+    
