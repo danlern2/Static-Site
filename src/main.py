91a44a7f603a553072c97044442401dd0db913e5
danlern2 <danlern2@gmail.com>
2024-06-15 12:54:28 -0700

    finished the copy function for real this time

--- src/main.py
+++ src/main.py
@@ -4,38 +4,31 @@ from markdown_to_nodes import *
 import os
 import shutil
 
-def directory_copier(dir_filepath_to_copy, new_dir_target, log=[]):
-    log = []
-    if os.path.exists(new_dir_target) == False:
-        os.mkdir(new_dir_target)
-    print(dir_filepath_to_copy)
+def directory_copier(dir_filepath_to_copy, new_dir_target):
+    head_tail = os.path.split(new_dir_target)
+    log = [head_tail[0]]
     if os.path.exists(dir_filepath_to_copy) == False:
         raise Exception("Not a valid filepath")
+    if os.path.exists(new_dir_target) == False and os.path.isfile(dir_filepath_to_copy) == False:
+        os.mkdir(new_dir_target)
     if os.path.isfile(dir_filepath_to_copy) == True:
             shutil.copy(dir_filepath_to_copy, new_dir_target)
-            log.append(new_dir_target)
+            log.append(head_tail[1])
             return log
     for item in os.listdir(dir_filepath_to_copy):
-        print(dir_filepath_to_copy)
         dir_path_to_copy = os.path.join(dir_filepath_to_copy, item)
         target_path = os.path.join(new_dir_target, item)
-        os.mkdir(target_path)
-        log.append(directory_copier(dir_path_to_copy, target_path, log))
-        continue
+        log.append(directory_copier(dir_path_to_copy, target_path))
     print(log)
     return log
     
 
 
-
-
-
-
-
 def main():
     copy = "/home/danlern2/bootdevworkspace/static_site/TESTDIRECTORY"
-    target = "/home/danlern2/bootdevworkspace/static_site/public/TESTFOLDER"
-    shutil.rmtree(target)
+    target = "/home/danlern2/bootdevworkspace/static_site/public/TESTFOLDER/"
+    if os.path.exists(target):
+        shutil.rmtree(target)
     directory_copier(copy, target)
     # print()
 main()
