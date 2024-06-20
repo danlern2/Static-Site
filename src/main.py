75ff928716acaabcd06a29c66f38f20ebb6bc465
Ji Park <ji.park@jpnws.com>
2024-06-19 20:37:27 -0700

    Fix pathing issue. (#7)

--- src/main.py
+++ src/main.py
@@ -1,24 +1,23 @@
-import pathlib
 import os
+import pathlib
 import shutil
+
 from copystatic import directory_copier
 from page_generator import generate_pages_recursive
 
 
 def main():
     content = pathlib.Path(
-        os.path.abspath(os.path.expanduser(os.path.expandvars("./static_site/content")))
+        os.path.abspath(os.path.expanduser(os.path.expandvars("content")))
     )
     static = pathlib.Path(
-        os.path.abspath(os.path.expanduser(os.path.expandvars("./static_site/static")))
+        os.path.abspath(os.path.expanduser(os.path.expandvars("static")))
     )
     template = pathlib.Path(
-        os.path.abspath(
-            os.path.expanduser(os.path.expandvars("./static_site/template.html"))
-        )
+        os.path.abspath(os.path.expanduser(os.path.expandvars("template.html")))
     )
     dest_path = pathlib.Path(
-        os.path.abspath(os.path.expanduser(os.path.expandvars("./static_site/public")))
+        os.path.abspath(os.path.expanduser(os.path.expandvars("public")))
     )
     print(f"Setting up directory at {dest_path}")
     if os.path.exists(dest_path):
